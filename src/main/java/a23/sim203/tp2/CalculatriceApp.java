8a710efd8407e4de2b99c90829223344c42fc650
EmileAntoineJayke <a@.com>
2023-10-02 13:57:17 -0400

    fxml sans etirement

--- src/main/java/a23/sim203/tp2/CalculatriceApp.java
+++ src/main/java/a23/sim203/tp2/CalculatriceApp.java
@@ -1,8 +1,25 @@
 package a23.sim203.tp2;
 
 
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class CalculatriceApp {
+import java.net.URL;
+
+public class CalculatriceApp extends Application {
 
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("calculatrice.fxml"));
+        Node root = fxmlLoader.load();
+        Scene scene = new Scene((Parent) root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
--- src/main/java/a23/sim203/tp2/controller/CalculatriceController.java
+++ src/main/java/a23/sim203/tp2/controller/CalculatriceController.java
@@ -1,10 +1,105 @@
 package a23.sim203.tp2.controller;
 
-import a23.sim203.tp2.modele.MoteurCalcul;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.ToggleButton;
 
 public class CalculatriceController {
 
-    private MoteurCalcul moteurCalcul = new MoteurCalcul();
+    @FXML
+    private Button bouton0;
 
+    @FXML
+    private Button bouton1;
+
+    @FXML
+    private Button bouton2;
+
+    @FXML
+    private Button bouton3;
+
+    @FXML
+    private Button bouton4;
+
+    @FXML
+    private Button bouton5;
+
+    @FXML
+    private Button bouton6;
+
+    @FXML
+    private Button bouton7;
+
+    @FXML
+    private Button bouton8;
+
+    @FXML
+    private Button bouton9;
+
+    @FXML
+    private Button boutonAdditionner;
+
+    @FXML
+    private Button boutonAjoute;
+
+    @FXML
+    private Button boutonDiviser;
+
+    @FXML
+    private Button boutonEffacer;
+
+    @FXML
+    private Button boutonEgal;
+
+    @FXML
+    private Button boutonMultiplier;
+
+    @FXML
+    private Button boutonParentheseD;
+
+    @FXML
+    private Button boutonParentheseG;
+
+    @FXML
+    private Button boutonPlusMinus;
+
+    @FXML
+    private Button boutonPoint;
+
+    @FXML
+    private Button boutonReculer;
+
+    @FXML
+    private Button boutonSoustraire;
+
+    @FXML
+    private Button boutonSupprime;
+
+    @FXML
+    private Button boutonVariable;
+
+    @FXML
+    private ListView<?> listeVariables;
+
+    @FXML
+    private ListView<?> listeÉquations;
+
+    @FXML
+    private Menu menuAide;
+
+    @FXML
+    private Menu menuAssistance;
+
+    @FXML
+    private CheckMenuItem menuItemAPropos;
+
+    @FXML
+    private CheckMenuItem menuItemAssistanceVisuelle;
+
+    @FXML
+    private ToggleButton toggleBoutonLire;
 
 }
