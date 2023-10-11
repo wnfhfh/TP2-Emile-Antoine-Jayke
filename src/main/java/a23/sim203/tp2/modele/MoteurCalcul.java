f983788aabb680ce2afb45a9d2421ded0f7b8896
JaykeGagne <126257547+JaykeGagne@users.noreply.github.com>
2023-10-11 10:59:57 -0400

    Les trois première méthodes de la classe MoteurCalcul sont commencés. Pour la 4ième, seulement son début.

--- src/main/java/a23/sim203/tp2/modele/MoteurCalcul.java
+++ src/main/java/a23/sim203/tp2/modele/MoteurCalcul.java
@@ -1,37 +1,46 @@
 package a23.sim203.tp2.modele;
 
+import javafx.scene.control.Alert;
 import org.mariuszgromada.math.mxparser.*;
 
+import javax.lang.model.element.VariableElement;
 import java.util.*;
 
 public class MoteurCalcul {
 
     // ajoutez les attributs pour stocker les équations et les variables
-
+    private Expression expression;
+    private Equation equation;
 
     public MoteurCalcul() {
         License.iConfirmNonCommercialUse("Cegep Limoilou");
+        this.expression = new Expression();
+
     }
 
 
     private Set<String> determineToutesVariablesRequises() {
-
-
-        return null; // à changer
+        return new HashSet<>(getToutesLesVariables());
     }
 
     private void ajouteVariable(String variable, double valeur) {
-
+        Constant constant = new Constant(variable, valeur);
+        expression.addConstants(constant);
     }
 
     public void setValeurVariable(String nomVariable, double valeur) {
-
+        Constant var = expression.getConstant(nomVariable);
+        var.setConstantValue(valeur);
     }
 
-
     public void ajouteEquation(String nouvelleEquation) {
-
-
+        Equation equation1 = new Equation(nouvelleEquation,expression.getDescription());
+        for (int i = 0; i < getToutesLesVariables().size() ; i++) {
+            if (expression.getConstant(i) == null){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Équation non valide");
+            }
+        }
     }
 
 
@@ -63,9 +72,6 @@ public class MoteurCalcul {
     }
 
 
-
-
-
     public Map<Object, Object> getVariableValueMap() {
 
         return null; // à changer
@@ -84,15 +90,15 @@ public class MoteurCalcul {
 
         License.iConfirmNonCommercialUse("Cegep Limoilou");
 
-        Constant A0 = new Constant("a0",3);
-        Constant B0 = new Constant("b0",3);
+        Constant A0 = new Constant("a0", 3);
+        Constant B0 = new Constant("b0", 3);
 
 
         Expression e1 = new Expression("3+4+a0+b0", A0, B0);
-        Expression e2 = new Expression("3+4+a0+b0", new Constant[]{A0,B0});// alternative avec tableau
+        Expression e2 = new Expression("3+4+a0+b0", new Constant[]{A0, B0});// alternative avec tableau
 
-        System.out.println("e1="+e1.calculate());
-        System.out.println("e2="+e2.calculate());
+        System.out.println("e1=" + e1.calculate());
+        System.out.println("e2=" + e2.calculate());
 
     }
 }
